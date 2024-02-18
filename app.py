14a265538ad07bd115e5c9d77ee0731cb6fbae66
Jefferson Teng <you@example.com>
2024-02-18 15:23:31 -0800

    start moving app toward chatbot

--- app.py
+++ app.py
@@ -12,36 +12,27 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    print(posts)
-    conn.close()
-    return render_template('index.html', posts=posts)
-
-@app.route('/create/', methods=('GET', 'POST'))
-def create():
-    print('beginning')
-    if request.method == 'POST':
-        title = request.form['title']
+    if request.method == 'GET':
+        posts = conn.execute('SELECT * FROM posts').fetchall()
+        print(posts)
+        conn.close()
+        return render_template('index.html', posts=posts)
+    else:
         content = request.form['content']
 
-        if not title:
-            flash('Title is required!')
-        elif not content:
+        if not content:
             flash('Content is required!')
         else:
             conn = get_db_connection()
             print('gets here')
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            conn.execute('INSERT INTO posts (content) VALUES (?)',
+                         (content,))
             conn.commit()
             conn.close()
-            return redirect(url_for('index'))
-
-    return render_template('create.html')
-
+            return redirect(url_for('index')) 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- init_db.py
+++ init_db.py
@@ -8,12 +8,11 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
+cur.execute("INSERT INTO posts (content) VALUES (?)",
+            ('First Query',)
             )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
+cur.execute("INSERT INTO posts (content) VALUES (?)",
+            ('Second Query',)
             )
 
 connection.commit()
